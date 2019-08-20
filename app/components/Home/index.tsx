97a6c19ceb144f0c822fba8c4c45755bbcc17118
Ethan Nguyen <thanhnx.605@gmail.com>
2019-08-20 17:05:05 +0700

    Refine

--- app/components/Home/index.tsx
+++ app/components/Home/index.tsx
@@ -1,16 +1,13 @@
 import React, { SFC } from 'react';
 import Head from 'next/head';
 
-// Locals
-import './styles.scss';
-
 const Home: SFC = () => (
   <div className="container my-2">
     <Head>
       <title>Home</title>
     </Head>
 
-    <h1 className="title">{'Tiny Quizzes'}</h1>
+    <h1 className="m-0 text-primary">{'Tiny Quizzes'}</h1>
   </div>
 );
 
